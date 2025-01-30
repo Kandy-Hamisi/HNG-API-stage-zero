c58be1d3936d658795a69a914b380ba9152d9e0c
Kandy-Hamisi <hkandy2000@gmail.com>
2025-01-30 14:07:20 +0300

    changed user info route and configured vercel.sjon

--- index.ts
+++ index.ts
@@ -6,11 +6,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // setup application middlewares
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
+
+app.get("/", (req, res) => {
+    res.send("<h1>My Stage Zero Api Test</h1>");
+});
+
 app.use(express.json());
 
 //routes
-app.use("/", stageZeroRoutes);
+app.use("/api/getUserinfo", stageZeroRoutes);
 
 // start the server
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
