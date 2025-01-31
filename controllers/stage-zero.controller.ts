ff90c9bda168eebc4edd5891d322e24574881d94
Kandy-Hamisi <hkandy2000@gmail.com>
2025-01-31 10:20:16 +0300

    removed extra underscore on datetime keyname

--- controllers/stage-zero.controller.ts
+++ controllers/stage-zero.controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 export const getUserInformation = (req: Request, res: Response)=> {
     const response = {
         email: "hkandy2000@gmail.com",
-        current__datetime: new Date().toISOString(),
+        current_datetime: new Date().toISOString(),
         github_url: "https://github.com/Kandy-Hamisi",
     };
 
