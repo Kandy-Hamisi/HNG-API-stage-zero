2cc9e31904ecac300f4996ccfed1096c3debde6b
Kandy-Hamisi <hkandy2000@gmail.com>
2025-01-31 10:32:12 +0300

    Removed the .git suffix on repo url

--- controllers/stage-zero.controller.ts
+++ controllers/stage-zero.controller.ts
@@ -4,7 +4,7 @@ export const getUserInformation = (req: Request, res: Response)=> {
     const response = {
         email: "hkandy2000@gmail.com",
         current_datetime: new Date().toISOString(),
-        github_url: "https://github.com/Kandy-Hamisi/HNG-API-stage-zero.git",
+        github_url: "https://github.com/Kandy-Hamisi/HNG-API-stage-zero",
     };
 
     res.status(200).json(response);
